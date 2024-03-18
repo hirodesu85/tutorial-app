813c2738aca79b065de9509e61f6f30f4943d672
hirodesu85 <hiromukkun@gmail.com>
2024-03-18 07:22:04 +0000

    fix:ホスト名変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'tutorial-app-hbrv.onrender.com'
+  host = 'tutorial-app.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
